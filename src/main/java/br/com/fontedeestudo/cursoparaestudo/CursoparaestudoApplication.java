88391778014f3fc1c8aca7213ba483d7483f8c6d
Lucas Lima <lucascampos07@hotmail.com>
2019-08-21 00:18:30 -0300

    projeto criado

---
+++ src/main/java/br/com/fontedeestudo/cursoparaestudo/CursoparaestudoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fontedeestudo.cursoparaestudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoparaestudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoparaestudoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fontedeestudo/cursoparaestudo/CursoparaestudoApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.fontedeestudo.cursoparaestudo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursoparaestudoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
